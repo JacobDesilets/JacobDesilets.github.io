a1d50fb551c2172c46a599119a3d726aef3ab8a7
JacobDesilets <jacobdesilets@gmail.com>
2023-06-14 03:27:24 -0400

    HEIC to png converter

---
+++ utils.js
@@ -0,0 +1,38 @@
+// https://vemto.app/blog/how-to-create-an-image-upload-viewer-with-alpinejs
+function imageViewer() {
+    return {
+        imageUrl: '', working: '',
+
+        fileChosen(event) {
+            this.working = 'yes';
+            this.fileToDataUrl(event, src => this.imageUrl = src);
+        },
+
+        fileToDataUrl(event, callback) {
+            if (!event.target.files.length) return;
+
+            let file = event.target.files[0],
+                reader = new FileReader();
+
+            heic2any({
+                blob: file
+            }).then(conversionResult => {
+                reader.readAsDataURL(conversionResult)
+                reader.onload = e => callback(e.target.result)
+                this.working = 'done';
+            }).catch(errorObject => {
+                console.log(errorObject);
+            });
+
+            
+        },
+    }
+}
+
+function copyEmail() {
+    navigator.clipboard.writeText('jacobdesilets@gmail.com');
+}
+
+function convertHeicToJpg() {
+
+}
